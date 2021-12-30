3c895fdae4bf784bdbe5e19dfa243eaa2709fbea
DumesAO <64592207+DumesAO@users.noreply.github.com>
2021-12-30 03:29:38 +0200

    Lab4

---
+++ app/src/main/java/com/example/Lab1/MainActivity.java
@@ -0,0 +1,86 @@
+package com.example.Lab1;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.MediaController;
+import android.widget.Toast;
+import android.widget.VideoView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button playBtn;
+    Button pauseBtn;
+    Button stopBtn;
+    VideoView player;
+    int currentPlaying=R.raw.video;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        player = findViewById(R.id.player);
+        Uri uri=Uri.parse( "android.resource://" + getPackageName() + "/" + currentPlaying);
+        player.setVideoURI(uri);
+
+        MediaController mediaController = new MediaController(this);
+        player.setMediaController(mediaController);
+
+        mediaController.setMediaPlayer(player);
+
+
+        playBtn = findViewById(R.id.playBtn);
+        pauseBtn = findViewById(R.id.pauseBtn);
+        stopBtn = findViewById(R.id.stopBtn);
+        pauseBtn.setEnabled(false);
+        stopBtn.setEnabled(false);
+    }
+    private void stopPlaying(){
+        pauseBtn.setEnabled(false);
+        stopBtn.setEnabled(false);
+        player.pause();
+        player.seekTo(0);
+        playBtn.setEnabled(true);
+    }
+
+
+    public void play(View view){
+
+        if(player.isPlaying()){
+            player.resume();
+        } else {
+            player.start();
+        }
+        playBtn.setEnabled(false);
+        pauseBtn.setEnabled(true);
+        stopBtn.setEnabled(true);
+    }
+    public void pause(View view){
+
+        player.pause();
+        playBtn.setEnabled(true);
+        pauseBtn.setEnabled(false);
+        stopBtn.setEnabled(true);
+    }
+    public void stop(View view){
+        stopPlaying();
+    }
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (player.isPlaying()) {
+            stopPlaying();
+        }
+    }
+
+    public void change(View view) {
+        currentPlaying= currentPlaying == R.raw.video ? R.raw.audio : R.raw.video;
+        Uri uri=Uri.parse( "android.resource://" + getPackageName() + "/" + currentPlaying);
+        player.setVideoURI(uri);
+    }
+}
